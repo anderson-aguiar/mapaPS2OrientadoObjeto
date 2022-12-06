b654afdacc96a8e7d85e06e648f777bdd2e33f19
Anderson Martins <anderson.aguiar@outlook.com.br>
2022-12-06 03:22:23 -0300

    Update class Employee

--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -3,9 +3,20 @@ package entities;
 import java.time.LocalDate;
 
 public class Employee extends Citizen {
+    protected String occupation;
+
     public Employee(String name, String cpf) {
         super(name, cpf);
     }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
 
     @Override
     public boolean validateDate(String date, String date2) {
